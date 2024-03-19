19f96242555c4d634d7e72b65d53bd499b0bd022
Muhammad Raza <105001837+completelyblank@users.noreply.github.com>
2024-03-19 18:19:44 +0500

    Update intro.c
    comments to explain

--- intro.c
+++ intro.c
@@ -3,23 +3,29 @@
 
 using namespace std;
 
-void openFile(GtkWidget *widget, gpointer data) {
+void openFile(GtkWidget *widget, gpointer data) //selects image file and displays in open window
+{
     GtkWidget *dialog;
+    //dialog = creates options for opening image and cancelling the process
     dialog = gtk_file_chooser_dialog_new("Open Image", NULL, GTK_FILE_CHOOSER_ACTION_OPEN, "_Cancel", GTK_RESPONSE_CANCEL, "_Open", GTK_RESPONSE_ACCEPT, NULL);
-    if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
-        char *filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
-        Mat image = imread(filename);
-        if (!image.empty()) {
-            namedWindow("Image", WINDOW_NORMAL);
-            gtk_widget_destroy(dialog);
-            resize(image, image, Size(900, 500));
-            imshow("Image", image);
-            waitKey(0);
-            destroyAllWindows();
+    if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) //if the open button is clicked
+    {
+        char *filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog)); //file selected and stored in c code
+        Mat image = imread(filename); //reads chosen image and stores in image variable
+        if (!image.empty()) //if image loaded sucessfully
+        {
+            namedWindow("Image", WINDOW_NORMAL); //image opened in window
+            gtk_widget_destroy(dialog); //file chooser window destroyed
+            resize(image, image, Size(900, 500)); //image resized
+            imshow("Image", image); //image shown after resizing
+            waitKey(0); //waits for user to interact
+            destroyAllWindows(); //windows closed
         }
-        g_free(filename);
-    } else {
-        gtk_widget_destroy(dialog);
+        g_free(filename); //memory freed
+    } 
+    else //image not loaded
+    {
+        gtk_widget_destroy(dialog); //file chooser closed (destroyed)
     }
 }
 
@@ -37,14 +43,14 @@ void callbackFunc(int event, int x, int y, int flags, void* userdata) {
 
     if (x > 295 && x < 600 && y > 335 && y < 410) {
         setCursor(gdk_get_default_root_window(), handCursor);
-        background = imread("src/Multedio Hover.png");
+        background = imread("Multedio Hover.png");
         if (event == EVENT_LBUTTONDOWN) {
             setCursor(gdk_get_default_root_window(), watchCursor);
             openFile(nullptr, nullptr);
         }
     } else {
         setCursor(gdk_get_default_root_window(), arrowCursor);
-        background = imread("src/Multedio.png"); 
+        background = imread("Multedio.png"); 
     }
 
     resize(background, background, Size(900, 500));
@@ -53,3 +59,4 @@ void callbackFunc(int event, int x, int y, int flags, void* userdata) {
     background.copyTo(backgroundROI);
     imshow("Main Window", mainWindow);
 }
+
