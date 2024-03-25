5022df001a3d5b883cab2de4bc17b5cbf9a825fb
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2024-03-25 13:36:31 +0500

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
 #include <opencv2/opencv.hpp>
 #include <gtk/gtk.h>
 #include "intro.h"
+#include "station.h"
+#include "upload.h"
 
 using namespace cv;
 
 Mat background; //image variable for background (Multedio's background that we show on opening)
+GdkCursor *handCursor;
+GdkCursor *arrowCursor;
+GdkCursor *watchCursor;
+gfloat width;
+gfloat height;
+gfloat scaledWidth;
+gfloat scaledHeight;
+Mat hovering[9];
+
 
 int main() 
 {  
