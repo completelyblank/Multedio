595a5e12a7d11f02865b80b0bef6f6c58a247766
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2024-03-27 22:46:24 +0500

    Update station.c

--- station.c
+++ station.c
@@ -13,11 +13,21 @@ void initializeHoveringPNGs() {
     	hovering[6] = imread("src/Vignette_Hover.png");
     	hovering[7] = imread("src/Brightness_Hover.png");
     	hovering[8] = imread("src/Color_Manipulation_Hover.png");
+    	
+    	filters[0] = imread("src/Temperature.png");
+    	filters[1] = imread("src/Tint.png");
+    	filters[2] = imread("src/Exposure.png");
+    	filters[3] = imread("src/Crop.png");
+    	filters[4] = imread("src/Preset.png");
+    	filters[5] = imread("src/Noise Reduction.png");
+    	filters[6] = imread("src/Vignette.png");
+    	filters[7] = imread("src/Brightness.png");
+    	filters[8] = imread("src/Color Grading.png");
 }
 
 void station(int event, int x, int y, int flags, void* userdata) {
 	// Load the background image
-    	Mat background = imread("src/Station.png");
+	static Mat background = imread("src/Station.png");
     	char *filename = (char*)userdata;
     	Mat image = imread(filename);
     	if (event == EVENT_LBUTTONDOWN) 
@@ -27,55 +37,127 @@ void station(int event, int x, int y, int flags, void* userdata) {
     	
     	if (y > 148*scaledHeight && y < 220*scaledHeight) {
 		if (x > 63*scaledWidth && x < 147*scaledHeight) {
-			background = hovering[0];
-			setCursor(gdk_get_default_root_window(), handCursor);
+			if (!clicked) {
+				background = hovering[0];
+				setCursor(gdk_get_default_root_window(), handCursor);
+				if (event == EVENT_LBUTTONDOWN) {
+					background = filters[0];
+					setCursor(gdk_get_default_root_window(), arrowCursor);
+					clicked = true;
+				}
+			}
 		} else if (x > 208*scaledWidth && x < 289*scaledHeight) {
-			background = hovering[1];
-			setCursor(gdk_get_default_root_window(), handCursor);
+			if (!clicked) {
+				background = hovering[1];
+				setCursor(gdk_get_default_root_window(), handCursor);
+				if (event == EVENT_LBUTTONDOWN) {
+					background = filters[1];
+					setCursor(gdk_get_default_root_window(), arrowCursor);
+					clicked = true;
+				}
+			}
 		} else if (x > 349*scaledWidth && x < 434*scaledHeight) {
-			background = hovering[2];
-			setCursor(gdk_get_default_root_window(), handCursor);
+			if (!clicked) {
+				background = hovering[2];
+				setCursor(gdk_get_default_root_window(), handCursor);
+				if (event == EVENT_LBUTTONDOWN) {
+					background = filters[2];
+					setCursor(gdk_get_default_root_window(), arrowCursor);
+					clicked = true;
+				}
+			}
 		} else {
 			setCursor(gdk_get_default_root_window(), arrowCursor);
 		}
 	} else if (y > 264*scaledHeight && y < 334*scaledHeight) {
 		if (x > 63*scaledWidth && x < 147*scaledHeight) {
-			background = hovering[3];
-			setCursor(gdk_get_default_root_window(), handCursor);
+			if (!clicked) {
+				background = hovering[3];
+				setCursor(gdk_get_default_root_window(), handCursor);
+				if (event == EVENT_LBUTTONDOWN) {
+					background = filters[3];
+					setCursor(gdk_get_default_root_window(), arrowCursor);
+					clicked = true;
+				}
+			}
 		} else if (x > 208*scaledWidth && x < 289*scaledHeight) {
-			background = hovering[4];
-			setCursor(gdk_get_default_root_window(), handCursor);
+			if (!clicked) {
+				background = hovering[4];
+				setCursor(gdk_get_default_root_window(), handCursor);
+				if (event == EVENT_LBUTTONDOWN) {
+					background = filters[4];
+					setCursor(gdk_get_default_root_window(), arrowCursor);
+					clicked = true;
+				}
+			}
 		} else if (x > 349*scaledWidth && x < 434*scaledHeight) {
-			background = hovering[5];
-			setCursor(gdk_get_default_root_window(), handCursor);
+			if (!clicked) {
+				background = hovering[5];
+				setCursor(gdk_get_default_root_window(), handCursor);
+				if (event == EVENT_LBUTTONDOWN) {
+					background = filters[5];
+					setCursor(gdk_get_default_root_window(), arrowCursor);
+					clicked = true;
+				}
+			}
 		} else {
 			setCursor(gdk_get_default_root_window(), arrowCursor);
 		}
 	} else if (y > 384*scaledHeight && y < 450*scaledHeight) {
 		if (x > 63*scaledWidth && x < 147*scaledHeight) {
-			background = hovering[6];
-			setCursor(gdk_get_default_root_window(), handCursor);
+			if (!clicked) {
+				background = hovering[6];
+				setCursor(gdk_get_default_root_window(), handCursor);
+				if (event == EVENT_LBUTTONDOWN) {
+					background = filters[6];
+					setCursor(gdk_get_default_root_window(), arrowCursor);
+					clicked = true;
+				}
+			}
 		} else if (x > 208*scaledWidth && x < 289*scaledHeight) {
-			background = hovering[7];
-			setCursor(gdk_get_default_root_window(), handCursor);
+			if (!clicked) {
+				background = hovering[7];
+				setCursor(gdk_get_default_root_window(), handCursor);
+				if (event == EVENT_LBUTTONDOWN) {
+					background = filters[7];
+					setCursor(gdk_get_default_root_window(), arrowCursor);
+					clicked = true;
+				}
+			}
 		} else if (x > 349*scaledWidth && x < 434*scaledHeight) {
-			background = hovering[8];
-			setCursor(gdk_get_default_root_window(), handCursor);
+			if (!clicked) {
+				background = hovering[8];
+				setCursor(gdk_get_default_root_window(), handCursor);
+				if (event == EVENT_LBUTTONDOWN) {
+					background = filters[8];
+					setCursor(gdk_get_default_root_window(), arrowCursor);
+					clicked = true;
+				}
+			}
 		} else {
 			setCursor(gdk_get_default_root_window(), arrowCursor);
 		}
 	} else {
 		setCursor(gdk_get_default_root_window(), arrowCursor);
 	}
-	 resize(background, background, Size(width*0.75, height*0.75));
-	 resize(image, image, Size(scaledWidth*470, scaledHeight*472));
+	
+	if (x > 52*scaledWidth && x < 92*scaledWidth && y > 143*scaledHeight && y < 182*scaledHeight && clicked == true) {
+		setCursor(gdk_get_default_root_window(), handCursor);
+		if (event == EVENT_LBUTTONDOWN) {
+			setCursor(gdk_get_default_root_window(), arrowCursor);
+			clicked = false;
+		}
+	}
+	
+	resize(background, background, Size(width*0.75, height*0.75));
+	resize(image, image, Size(470, 472));
 
 	// Create a main window for displaying the result
     	Mat mainWindow(height*0.75, width*0.75, CV_8UC3, Scalar(51, 138, 255));
 
     	// Define the position to place the image on top of the background
-    	int imageX = scaledWidth * 520; // Adjust these values as needed
-    	int imageY = scaledHeight * 16; // Adjust these values as needed
+    	int imageX = 520; // Adjust these values as needed
+    	int imageY = 16; // Adjust these values as needed
     
         Rect imageROI(imageX, imageY, image.cols, image.rows);
         
