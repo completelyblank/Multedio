591120432efca0f65a60aaabbc5920676731ec17
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2024-04-16 08:18:44 +0500

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include "intro.h"
 #include "station.h"
 #include "upload.h"
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 using namespace cv;
 
@@ -17,8 +20,28 @@ gfloat scaledHeight;
 Mat hovering[9];
 Mat filters[9];
 bool clicked;
+bool slider;
+int sliderValue;
+bool dragging;
+int threadCreated;
+pthread_t threadID;
+int mouseCordx;
+int mouseEvent;
+pthread_mutex_t threadMutex;
+int flag1, flag2, flag3, flag4;
 
 int main() 
 {  
-	mainWindow();
+	flag1 = false;
+	flag2 = false;
+	flag3 = false;
+	flag4 = false;
+	pthread_t ui, other;
+	pthread_create(&ui, NULL, mainWindow, NULL);
+	pthread_create(&other, NULL, mainOther, NULL);
+	pthread_join(ui, NULL);
+	pthread_join(other, NULL);
 }
+
+
+
