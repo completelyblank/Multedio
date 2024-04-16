edc7c14d52dac582ce302e5bc3dc3c69ec9490cf
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2024-04-16 19:13:19 +0500

    Update slider.c

--- slider.c
+++ slider.c
@@ -5,7 +5,7 @@ using namespace std;
 
 void drawSlider(Mat &image, int value) {
     	resize(image, image, Size(width*0.75, height*0.75));
-        rectangle(image, Point(110, 300), Point(380, 320), Scalar(255, 255, 255), -1);
+        rectangle(image, Point(140, 310), Point(350, 330), Scalar(255, 255, 255), -1);
    	
     	// Draw the trackbar slider
     	line(image, Point(value, 290), Point(value, 330), Scalar(0, 0, 255), 2);
@@ -17,9 +17,6 @@ void onMouse(int event, int x, int y, int flags, void* userdata) {
 
 void mouseCallBack() {
 		drawSlider(background, sliderValue);
-		if (mouseEvent == EVENT_LBUTTONDOWN) {
-        		sliderValue += 5;
-    		}
     		if(!clicked) {
     			return;
     		}
