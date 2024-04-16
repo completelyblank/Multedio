4254637bb99665536eb5405b65e06b372d2859d0
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2024-04-16 08:19:22 +0500

    Update station.c

--- station.c
+++ station.c
@@ -1,6 +1,14 @@
 #include "station.h"
+#include "slider.h"
+#include <pthread.h>
 #include <iostream>
 using namespace std;
+
+GtkWidget *scale;
+
+void on_scale_changed(int, void*) {
+
+}
 
 void initializeHoveringPNGs() {
 	// load assets
@@ -23,13 +31,18 @@ void initializeHoveringPNGs() {
     	filters[6] = imread("src/Vignette.png");
     	filters[7] = imread("src/Brightness.png");
     	filters[8] = imread("src/Color Grading.png");
+    	threadCreated = 0;
 }
 
 void station(int event, int x, int y, int flags, void* userdata) {
-	// Load the background image
-	static Mat background = imread("src/Station.png");
+	mouseCordx = x;
+	mouseEvent = event;
     	char *filename = (char*)userdata;
     	Mat image = imread(filename);
+    	if (event == EVENT_MOUSEMOVE) 
+    	{
+    		flag3 = true;
+    	}
     	if (event == EVENT_LBUTTONDOWN) 
     	{
         	cout << "Mouse clicked at: (" << x << ", " << y << ")" << endl;
@@ -135,9 +148,15 @@ void station(int event, int x, int y, int flags, void* userdata) {
 				}
 			}
 		} else {
+			if (!clicked) {
+				background = imread("src/Station.png");
+			}
 			setCursor(gdk_get_default_root_window(), arrowCursor);
 		}
 	} else {
+		if (!clicked) {
+			background = imread("src/Station.png");
+		}
 		setCursor(gdk_get_default_root_window(), arrowCursor);
 	}
 	
@@ -146,18 +165,24 @@ void station(int event, int x, int y, int flags, void* userdata) {
 		if (event == EVENT_LBUTTONDOWN) {
 			setCursor(gdk_get_default_root_window(), arrowCursor);
 			clicked = false;
+			slider = false;
+			threadCreated = 2;
 		}
 	}
 	
+	if(clicked) {
+		mouseCallBack();
+	}
+	
 	resize(background, background, Size(width*0.75, height*0.75));
-	resize(image, image, Size(470*scaledWidth, 472*scaledHeight));
+	resize(image, image, Size(470, 472));
 
 	// Create a main window for displaying the result
     	Mat mainWindow(height*0.75, width*0.75, CV_8UC3, Scalar(51, 138, 255));
 
     	// Define the position to place the image on top of the background
-    	int imageX = 520*scaledWidth; // Adjust these values as needed
-    	int imageY = 16*scaledHeight; // Adjust these values as needed
+    	int imageX = 520; // Adjust these values as needed
+    	int imageY = 16; // Adjust these values as needed
     
         Rect imageROI(imageX, imageY, image.cols, image.rows);
         
@@ -173,4 +198,12 @@ void station(int event, int x, int y, int flags, void* userdata) {
 
 	// Show the result
     	imshow("Multedio", mainWindow);
+}
+
+void rendering() {
+	while (!flag4) {
+        	cout << "next next next other" << endl;
+        	
+        	usleep(1000000);
+	}
 }
