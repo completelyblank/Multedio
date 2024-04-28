328010fe0cfec4e606050efa0c362d383c78df61
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2024-04-28 17:03:48 +0500

    Update intro.h

--- intro.h
+++ intro.h
@@ -5,10 +5,25 @@
 #include <gtk/gtk.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <map>
+#include <functional>
 
 using namespace cv;
+using namespace std;
 
+#define READ_END 0
+#define WRITE_END 1
+
+extern char *ptr1, *ptr6;
+extern int *ptr2, *ptr3, *ptr4;
+extern Mat *ptr5;
 extern Mat background;
+extern int draw;
 extern GdkCursor *handCursor;
 extern GdkCursor *arrowCursor;
 extern GdkCursor *watchCursor;
@@ -18,16 +33,21 @@ extern gfloat scaledWidth;
 extern gfloat scaledHeight;
 extern Mat hovering[9];
 extern Mat filters[9];
-extern bool clicked;
+extern int clicked[2];
 extern bool slider;
-extern int sliderValue;
+extern int sliderValue[9];
 extern bool dragging;
 extern int threadCreated;
 extern pthread_t threadID;
-extern int mouseCordx;
+extern int imageRender;
 extern pthread_mutex_t threadMutex;
 extern int mouseEvent;
-extern int flag1, flag2, flag3, flag4;
+extern char* flag;
+extern pthread_t threadIDs[9];
+extern Mat image;
+extern int fd[2];
+extern bool written;
+extern map<string, function<void(Mat&, int)>> functionMap;
 
 char* openFile(GtkWidget *widget, gpointer data);
 void setCursor(GdkWindow *window, GdkCursor *cursor);
@@ -35,7 +55,7 @@ void callbackFunc(int event, int x, int y, int flags, void* userdata);
 void backgroundCode();
 void initializeCursors();
 void show(Mat background);
-void *mainWindow(void *arg);
-void *mainOther(void *arg);
+void mainWindow();
+void mainOther();
 
 #endif 
