4bfe64b35a1f6dd957c61142a96882f2e95f7c31
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2024-05-05 20:38:00 +0500

    Update tint.c

--- tint.c
+++ tint.c
@@ -5,12 +5,12 @@
 using namespace cv;
 using namespace std;
 
-void adjustTint(Mat& imageInput, int sliderValue) {
+void adjustTint(Mat& imageInput, int sliderValue, int previous) {
     size_t imageSize = imageInput.total() * imageInput.elemSize();
 
     // Scale slider value to range -1.0 to 1.0 for green and purple factors
-    double greenFactor = (245 - sliderValue) / 400.0;
-    double purpleFactor = (245 - sliderValue) / 400.0;
+    double greenFactor = (previous - sliderValue) / 400.0;
+    double purpleFactor = (previous - sliderValue) / 400.0;
 
     // Debug print
     cout << "Green Factor: " << greenFactor << endl;
@@ -22,7 +22,25 @@ void adjustTint(Mat& imageInput, int sliderValue) {
         pixel[1] = saturate_cast<uchar>(pixel[1] * (1.0 - greenFactor)); // Adjust green channel //RIGHT PEI GREEN KAREGA
         pixel[0] = saturate_cast<uchar>(pixel[0] * (1.0 + purpleFactor)); // Adjust blue (assuming purple) channel //LEFT PEI PURPLE
     });
+    
 
     // Update shared memory with adjusted image
     memcpy(ptr5, adjustedImage.data, imageSize);
+}
+
+void adjustTintPreset(int sliderValue, int previous) {
+    size_t imageSize = copyPresetImage.total() * copyPresetImage.elemSize();
+
+    // Scale slider value to range -1.0 to 1.0 for green and purple factors
+    double greenFactor = (previous - sliderValue) / 400.0;
+    double purpleFactor = (previous - sliderValue) / 400.0;
+
+    // Debug print
+    cout << "Green Factor: " << greenFactor << endl;
+    cout << "Purple Factor: " << purpleFactor << endl;
+
+    copyPresetImage.forEach<Vec3b>([&](Vec3b& pixel, const int* position) {
+        pixel[1] = saturate_cast<uchar>(pixel[1] * (1.0 - greenFactor)); // Adjust green channel //RIGHT PEI GREEN KAREGA
+        pixel[0] = saturate_cast<uchar>(pixel[0] * (1.0 + purpleFactor)); // Adjust blue (assuming purple) channel //LEFT PEI PURPLE
+    });
 }
