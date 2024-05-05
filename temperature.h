cbab01b11a2e294c54415f05fed00390bb4aef46
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2024-05-05 20:37:36 +0500

    Update temperature.h

--- temperature.h
+++ temperature.h
@@ -5,6 +5,7 @@
 #include <gtk/gtk.h>
 #include "intro.h"
 
-void adjustTemperature(Mat& image, int sliderValue);
+void adjustTemperature(Mat& image, int sliderValue, int previous);
+void adjustTemperaturePreset(int sliderValue, int previous);
 
 #endif
