d47fcefde1dbc42def6f7d8c8a981267375ffd6e
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2024-05-05 20:38:20 +0500

    Update tint.h

--- tint.h
+++ tint.h
@@ -5,6 +5,7 @@
 #include <gtk/gtk.h>
 #include "intro.h"
 
-void adjustTint(Mat& image, int sliderValue);
+void adjustTint(Mat& image, int sliderValue, int previous);
+void adjustTintPreset(int sliderValue, int previous);
 
 #endif
