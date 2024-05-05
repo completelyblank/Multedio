cc8e6c19e12157651e0de7d9a53f08a7fb979b29
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2024-05-05 20:35:25 +0500

    Update exposure.c

--- exposure.c
+++ exposure.c
@@ -5,14 +5,21 @@
 using namespace cv;
 using namespace std;
 
-void adjustExposure(Mat& imageInput, int sliderValue) {
+void adjustExposure(Mat& imageInput, int sliderValue, int previous) {
 	// Calculate image size
 	size_t imageSize = imageInput.total() * imageInput.elemSize();
-    	double brightnessFactor = (sliderValue - 245) / 100.0; // Scale slider value to range -1.0 to 1.0
-    	cout << "Brightness Factor: " << brightnessFactor << endl; // Debug print
+    	double exposureFactor = (sliderValue - previous) / 200.0; // Scale slider value to range -1.0 to 1.0
+    	cout << "Exposure Factor: " << exposureFactor << endl; // Debug print
     	Mat adjustedImage;
-   	imageInput.convertTo(adjustedImage, -1, 1.0 + brightnessFactor, 0);
+   	imageInput.convertTo(adjustedImage, -1, 1.0 + exposureFactor, 0);
     	// Update shared memory with adjusted image
     	memcpy(ptr5, adjustedImage.data, imageSize);
 }
 
+void adjustExposurePreset(int sliderValue, int previous) {
+	// Calculate image size
+	size_t imageSize = copyPresetImage.total() * copyPresetImage.elemSize();
+    	double exposureFactor = (sliderValue - previous) / 200.0; // Scale slider value to range -1.0 to 1.0
+    	cout << "Exposure Factor: " << exposureFactor << endl; // Debug print
+   	copyPresetImage.convertTo(copyPresetImage, -1, 1.0 + exposureFactor, 0);
+}
