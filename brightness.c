8624047fa930f9a818c69a4d65328402ab047278
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2024-05-05 20:33:51 +0500

    Update brightness.c

--- brightness.c
+++ brightness.c
@@ -5,10 +5,12 @@
 using namespace cv;
 using namespace std;
 
-void adjustBrightness(Mat& imageInput, int sliderValue) {
+void adjustBrightness(Mat& imageInput, int sliderValue, int previous) {
 	// Calculate image size
+	cout << "previous: " << sliderValue << endl; 
 	size_t imageSize = imageInput.total() * imageInput.elemSize();
-    	double brightnessFactor = (sliderValue - 245) / 100.0; // Scale slider value to range -1.0 to 1.0
+    	double brightnessFactor = static_cast<double>(sliderValue - previous) / 100.0;
+
     	cout << "Brightness Factor: " << brightnessFactor << endl; // Debug print
     	Mat adjustedImage;
    	imageInput.convertTo(adjustedImage, -1, 1.0 + brightnessFactor, 0);
@@ -16,3 +18,10 @@ void adjustBrightness(Mat& imageInput, int sliderValue) {
     	memcpy(ptr5, adjustedImage.data, imageSize);
 }
 
+void adjustBrightnessPreset(int sliderValue, int previous) {
+	size_t imageSize = copyPresetImage.total() * copyPresetImage.elemSize();
+    	double brightnessFactor = static_cast<double>(sliderValue - previous) / 100.0;
+
+    	cout << "Brightness Factor: " << brightnessFactor << endl; // Debug print
+   	copyPresetImage.convertTo(copyPresetImage, -1, 1.0 + brightnessFactor, 0);
+}
